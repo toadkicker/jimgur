1e4ef88188f5486f9314192e44143311605d70fa
Todd Baur <todd@toadkicker.com>
2013-06-21 14:03:08 -0700

    scaffold

--- jquery.jimgur.js
+++ jquery.jimgur.js
@@ -1,9 +1,26 @@
 (function($) {
   $.fn.jimgur = function(opts) {
-    var settings = $.extend({}, opts)
-      
+    var settings = $.extend({
+      apiURL: "https://api.imgur.com/3/"
+    }, opts),
+    image = {
+      get: function() { },
+      post: function(data) { },
+      put: function(data) { },
+      del: function() { }
+    },
+    comment = {
+      get: function() { },
+      post: function(data) { },
+      put: function(data) { },
+      del: function() { }
+    },
+    album = {
+      get: function() { },
+      post: function(data) { },
+      put: function(data) { },
+      del: function() { }
+    }
   };
-  
-
 })(jQuery);
 // test comment
